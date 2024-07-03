77c564cb1dcfe8d3dc685b972037834822bb8d1e
MRDonald1213 <133694865+MRDonald1213@users.noreply.github.com>
2024-07-03 20:32:55 +0400

    Changed Home-Page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -3,13 +3,18 @@ import styles from "./Home.module.scss";
 import background from "../../assets/backgorund.png";
 import Header from "../../components/Header/Header";
 import Background from "../../components/background/Background";
+import Logo from '../../assets/Logo.png'
 
 const Home = () => {
   return (
-    <div className={styles.wrapper}>
+    <>
       <Background />
       <Header />
-    </div>
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>rtrtrtr</h1>
+        <img className= {styles.logo} src={Logo} alt="" />
+      </div>
+    </>
   );
 };
 
