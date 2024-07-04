76841016fc6cc19001114a93ce6a91828e34fc00
MRDonald1213 <133694865+MRDonald1213@users.noreply.github.com>
2024-07-04 11:10:44 +0400

    updated Home page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -5,17 +5,24 @@ import Header from "../../components/Header/Header";
 import Background from "../../components/background/Background";
 import Logo from '../../assets/Logo.png'
 
+
 const Home = () => {
   return (
     <>
       <Background />
       <Header />
       <div className={styles.wrapper}>
-        <h1 className={styles.title}>rtrtrtr</h1>
+        
         <img className= {styles.logo} src={Logo} alt="" />
+        <div>
+        <p className= {styles.paragraph}> Smth to type</p>
+        <p className= {styles.paragraph}>Smth to type</p>
+        </div>
+        <div className= {styles.button}>Join us</div>
       </div>
     </>
   );
 };
 
 export default Home;
+
